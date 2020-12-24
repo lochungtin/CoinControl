f389a54daf4f00d4570d62faf4e6bbc88aa579f8
Issac To <issacto606@gmail.com>
2020-12-24 11:37:32 +0000

    Modification for google signin

--- src/screens/accountScreen.js
+++ src/screens/accountScreen.js
@@ -8,7 +8,7 @@ import auth from '@react-native-firebase/auth';
 
 
 GoogleSignin.configure({
-  webClientId: '1:486441035059:android:7c02c9a8d0fd6b0f268a46',
+  webClientId: '486441035059-8l61ntdopa47itlm7kknd6acpvmn04q4.apps.googleusercontent.com',
 });
 
     
@@ -20,9 +20,30 @@ class Screen extends React.Component {
     async  googleLogin() {
         const { idToken } = await GoogleSignin.signIn();
         const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+        console.log("fjoabi",googleCredential);
         return auth().signInWithCredential(googleCredential);
         
       }
+    onLoginOrRegister = () => {
+    GoogleSignin.signIn()
+        .then((data) => {
+        // Create a new Firebase credential with the token
+        const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+        // Login with the credential
+        return firebase.auth().signInWithCredential(credential);
+        })
+        .then((user) => {
+        // If you need to do anything with the user, do it here
+        // The user will be logged in automatically by the
+        // `onAuthStateChanged` listener we set up in App.js earlier
+        })
+        .catch((error) => {
+        const { code, message } = error;
+        // For details of error codes, see the docs
+        // The message contains the default Firebase string
+        // representation of the error
+        });
+    }
 
     render() {
         return (
