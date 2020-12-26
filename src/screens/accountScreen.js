e94cb2a246c7c8b3a0b77a176a9e490e405b802a
Issac To <issacto606@gmail.com>
2020-12-26 21:00:52 +0000

    Google Sign in Done

--- src/screens/accountScreen.js
+++ src/screens/accountScreen.js
@@ -1,49 +1,70 @@
 import React, {useState, useEffect} from 'react';
 import { Text, View, Button} from 'react-native';
 import { connect } from 'react-redux';
-
 import { styles } from '../styles';
-import { GoogleSignin } from '@react-native-community/google-signin';
-import auth,{firebase} from '@react-native-firebase/auth';
 
-GoogleSignin.configure({
-  webClientId: '486441035059-mqd6rf0e178kn2uke7k2pfdtii632qb1.apps.googleusercontent.com', offlineAccess: false,
-   
-});
 
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+  } from '@react-native-community/google-signin';
+
+
+  GoogleSignin.configure({
+    webClientId: '486441035059-8l61ntdopa47itlm7kknd6acpvmn04q4.apps.googleusercontent.com'
+  });
+  
 class Screen extends React.Component {
 
     constructor(props) {
         super(props);
     }
-      _signIn = async () => {
+    
+    signIn = async () => {
         try {
-          await GoogleSignin.hasPlayServices();
-          const {accessToken, idToken} = await GoogleSignin.signIn();
-          const credential = auth.GoogleAuthProvider.credential(
-            idToken,
-            accessToken,
-          );
-          await auth().signInWithCredential(credential);
-        } catch (error) {console.log(error)}}
-
-    async  onGoogleButtonPress() {
-        // Get the users ID token
-        const { idToken } = await GoogleSignin.signIn();
-
-    // Create a Google credential with the token
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-    // Sign-in the user with the credential
-    // googleCredential.token = googleCredential.token.idToken;
-
-    return auth().signInWithCredential(googleCredential);
+          await GoogleSignin.hasPlayServices(); 
+          const userInfo = await GoogleSignin.signIn();
+          console.log(userInfo);
+        } catch (error) {
+          if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+            console.log("SIGN_IN_CANCELLED")
+          } else if (error.code === statusCodes.IN_PROGRESS) {
+            console.log("IN_PROGRESS")
+            // operation (e.g. sign in) is in progress already
+          } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            console.log("PLAY_SERVICES_NOT_AVAILABLE")
+            // play services not available or outdated
+          } else {
+            console.log("AJ FK")
+            // some other error happened
+          }
         }
-
+      };
+      signOut = async () => {
+        try {
+          await GoogleSignin.revokeAccess();
+          await GoogleSignin.signOut();
+          this.setState({ user: null }); // Remember to remove the user from your app's state as well
+          console.log("HEre");
+        } catch (error) {
+            console.log("THIS");
+          console.error(error);
+        }
+      };
+   
     render() {
         return (
             <View style={styles.screen}>
                 <Text>Account Screen</Text>
-                <Button onPress={this._signIn } title="googleLogin">
+                <GoogleSigninButton
+                style={{ width: 192, height: 48 }}
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={this.signIn} />
+
+                <Button onPress={this.signOut } title="LogOut">
                 </Button>
             </View>
         );
