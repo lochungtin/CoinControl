45f6fbab9574c6b278024320043216961f0aaa5b
lochungtin <lochungtin@gmail.com>
2021-02-06 01:30:33 +0800

    android RN vector icons linked

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Icon name={'plus'} color={'#000000'} size={35} />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View>
+                <Icon name={'plus'} color={'#000000'} size={35} />
+            </View>
+        );
+    }
 }
