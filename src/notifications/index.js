5c277279f4d42e0faa6352fae1c453199e89914b
lochungtin <lochungtin@gmail.com>
2021-02-14 17:34:04 +0800

    refactor notif code

--- src/notifications/index.js
+++ src/notifications/index.js
@@ -20,6 +20,14 @@ export default class NotifService {
         PushNotification.getChannels(channels => console.log(channels));
     }
 
+    abandonPermissions = () => PushNotification.abandonPermissions();
+
+    cancelAll = () => PushNotification.cancelAllLocalNotifications();
+
+    cancelNotif = () => PushNotification.cancelLocalNotifications({ id: '' + this.lastId });
+
+    checkPermission = cbk => PushNotification.checkPermissions(cbk);
+
     createDefaultChannels = () => PushNotification.createChannel(
         {
             channelId: 'CCDailyNotif',
@@ -32,55 +40,41 @@ export default class NotifService {
         created => console.log(`createChannel 'CCDailyNotif' returned '${created}'`)
     );
 
-
-    scheduleNotif = (time, color) => {
-        PushNotification.localNotificationSchedule({
-            date: new Date(time),
-
-            // Android Only Properties
-            channelId: 'CCDailyNotif',
-            ticker: 'CCTicker',
-            autoCancel: true,
-            largeIcon: 'ic_launcher',
-            smallIcon: 'ic_notification',
-            color: color,
-            vibrate: true,
-            vibration: 300,
-            group: 'CCGroup',
-            groupSummary: false,
-            ongoing: false,
-            invokeApp: true,
-            when: null,
-            usesChronometer: false,
-            timeoutAfter: null,
-
-            // iOS only properties
-            category: '',
-
-            // iOS and Android properties
-            id: this.lastId++,
-            title: 'Daily Reminder',
-            message: "Don't forget to manage today's expense!",
-            userInfo: { sceen: 'home' },
-            playSound: true,
-            soundName: 'default',
-            repeatType: 'day',
-            number: 10,
-        });
-    }
-
-
-    abandonPermissions = () => PushNotification.abandonPermissions();
-
-    cancelAll = () => PushNotification.cancelAllLocalNotifications();
-
-    cancelNotif = () => PushNotification.cancelLocalNotifications({ id: '' + this.lastId });
-
-    checkPermission = cbk => PushNotification.checkPermissions(cbk);
-
     getScheduledLocalNotifications = callback => PushNotification.getScheduledLocalNotifications(callback);
 
     popInitialNotification = () => PushNotification.popInitialNotification((notification) => console.log('InitialNotication:', notification));
 
     requestPermissions = () => PushNotification.requestPermissions();
+
+    scheduleNotif = (time, color) => PushNotification.localNotificationSchedule({
+        date: new Date(time),
+        id: this.lastId++,
+        title: 'Daily Reminder',
+        message: "Don't forget to manage today's expense!",
+        userInfo: { sceen: 'home' },
+        playSound: true,
+        soundName: 'default',
+        repeatType: 'day',
+        number: 10,
+
+        // Android Only Properties
+        channelId: 'CCDailyNotif',
+        ticker: 'CCTicker',
+        autoCancel: true,
+        largeIcon: 'ic_launcher',
+        smallIcon: 'ic_notification',
+        color: color,
+        vibrate: true,
+        vibration: 300,
+        group: 'CCGroup',
+        groupSummary: false,
+        ongoing: false,
+        invokeApp: true,
+        when: null,
+        usesChronometer: false,
+        timeoutAfter: null,
+
+        // iOS only properties
+        category: '',
+    });
 }
