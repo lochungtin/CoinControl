4d7c8aa7ba3846be658d4c7cb3933ee2abfdd92d
lochungtin <lochungtin@gmail.com>
2021-02-24 14:11:35 +0800

    remove watch card

--- src/screens/chartScreen.js
+++ src/screens/chartScreen.js
@@ -7,7 +7,6 @@ import GoalCard from '../components/Cards/GoalCard';
 import PieCard from '../components/Cards/PieCard';
 import TitleCard from '../components/Cards/TitleCard';
 import TrendCard from '../components/Cards/TrendCard';
-import WatchCard from '../components/Cards/WatchCard';
 import CardModal from '../components/Modals/CardModal';
 
 import { update } from '../functions/GenStats';
@@ -49,7 +48,6 @@ class Screen extends React.Component {
                         />
                         {this.props.cards.tc && <TrendCard data={this.state.data.recent} />}
                         {this.props.cards.pc && <PieCard data={this.state.data.categories} total={this.state.data.total} />}
-                        {this.props.cards.wc && <WatchCard data={this.state.data.categories} total={this.state.data.total} />}
                         {this.props.cards.gc && <GoalCard />}
                         {this.props.cards.cc && <CategoryCard data={this.state.data.categories} total={this.state.data.total} />}
                     </ScrollView>
