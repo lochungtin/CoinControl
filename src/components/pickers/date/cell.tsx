fca741ed017dfada2ff29a47f0725f4d1927e89a
lochungtin <lochungtin@gmail.com>
2021-07-13 10:19:33 +0100

    component abstration

--- src/components/pickers/date/cell.tsx
+++ src/components/pickers/date/cell.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 
-import { ReduxPropType } from '../../types/redux';
+import { ReduxPropType } from '../../../types/redux';
 
-class Picker extends React.Component<ReduxPropType> {
+class Cell extends React.Component<ReduxPropType> {
     render() {
         return (
             <View>
@@ -18,4 +18,4 @@ const mapStateToProps = (state: ReduxPropType) => ({
     settings: state.settings,
 });
 
-export default connect(mapStateToProps)(Picker);
+export default connect(mapStateToProps)(Cell);
